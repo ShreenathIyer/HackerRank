7f6bf6a4e17e4498bb6e880675243fc32fd3aa4d
Shreenath Iyer <shreenath50@gmail.com>
2017-04-17 18:22:09 +0530

    Update apple_orange.py
    Add problem description

--- apple_orange.py
+++ apple_orange.py
@@ -1,5 +1,41 @@
 #!/bin/python
 
+'''
+Input Format:
+
+The first line contains two space-separated integers denoting the respective values of s and t. 
+The second line contains two space-separated integers denoting the respective values of a and b. 
+The third line contains two space-separated integers denoting the respective values of m and n. 
+The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point a. 
+The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point b.
+
+Constraints:
+
+1 <= s,t,a,b,m,n <= 10^5
+-10^5 <= d <= 10^5
+a < s < t < b
+
+Output Format:
+
+Print two lines of output:
+
+1) On the first line, print the number of apples that fall on Sam's house.
+2) On the second line, print the number of oranges that fall on Sam's house.
+
+Sample Input 0:
+
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+
+Sample Output 0:
+
+1
+1
+'''
+
 import sys
 
 
