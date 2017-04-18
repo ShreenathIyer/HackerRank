137b3de6745a63da725bbd2661cd3410cde4a533
Shreenath Iyer <shreenath50@gmail.com>
2017-04-18 13:02:40 +0530

    Update diagonal_difference.py

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,3 +1,29 @@
+'''
+Given a square matrix of size N * N, calculate the absolute difference between the sums of its diagonals.
+
+Input Format:
+
+The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line containing N space-separated integers describing the columns.
+
+Constraints:
+
+-100 <= Elements in the matrix <= 100
+
+Output Format:
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+
+Sample Input:
+
+3
+11 2 4
+4 5 6
+10 8 -12
+
+Sample Output:
+
+15
+'''
 #!/bin/python
 
 import sys
